c294d8d4d63fedcf7b52315e3d60d3dda75a6947
q1584428087@163.com <q1584428087@163.com>
2019-12-16 20:07:50 +0800

    格式化

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,24 +4,26 @@ import function as f
 from chess import Chess
 from button import Button
 
+
 def run_game():
-    pygame.init()   #初始化
-    screen = pygame.display.set_mode((500,600)) #500*600的屏幕
-    pygame.display.set_caption('Chess')         #标题
-    
-    image = pygame.image.load('images/bg.bmp') #载入界面
+    pygame.init()  # 初始化
+    screen = pygame.display.set_mode((500, 600))  # 500*600的屏幕
+    pygame.display.set_caption('Chess')  # 标题
+
+    image = pygame.image.load('images/bg.bmp')  # 载入界面
     chess = Chess()
-    retract_button = Button(screen,'Retract',200,100)   #悔棋按钮
-    replay_button = Button(screen,'Replay',200,50)      #重玩按钮
-    
+    retract_button = Button(screen, 'Retract', 200, 100)  # 悔棋按钮
+    replay_button = Button(screen, 'Replay', 200, 50)  # 重玩按钮
+
     while True:
-        screen.blit(image,(0,0))    #在(0,0)绘制底层图像
-        
-        f.check_keydown(chess,screen,retract_button,replay_button)#检测鼠标按动
-        f.draw(chess,screen)    #绘制棋子，框
-        
-        retract_button.draw_button()    #绘制悔棋按钮
-        replay_button.draw_button()     #绘制重玩按钮
-        pygame.display.update()     #更新屏幕
-        
+        screen.blit(image, (0, 0))  # 在(0,0)绘制底层图像
+
+        f.check_keydown(chess, screen, retract_button, replay_button)  # 检测鼠标按动
+        f.draw(chess, screen)  # 绘制棋子，框
+
+        retract_button.draw_button()  # 绘制悔棋按钮
+        replay_button.draw_button()  # 绘制重玩按钮
+        pygame.display.update()  # 更新屏幕
+
+
 run_game()
--- function.py
+++ function.py
@@ -5,41 +5,43 @@ import chess
 
 center_x = 250
 center_y = 400
-#棋盘的中心位置
 
-def check_keydown(chess,screen,retract_button,replay_button):
+
+# 棋盘的中心位置
+
+def check_keydown(chess, screen, retract_button, replay_button):
     '''检测按键'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    
+
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x,mouse_y = pygame.mouse.get_pos()
-            check_position(mouse_x,mouse_y,chess)
-            check_button(screen,retract_button,replay_button,  
-                chess,mouse_x,mouse_y)
-        
-            
-def check_position(mouse_x,mouse_y,chess):
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            check_position(mouse_x, mouse_y, chess)
+            check_button(screen, retract_button, replay_button,
+                         chess, mouse_x, mouse_y)
+
+
+def check_position(mouse_x, mouse_y, chess):
     '''产生点击的坐标'''
     a = ''
     position1 = ''
     position2 = []
-    
+
     if mouse_x >= center_x - 142.5 and mouse_x <= center_x - 52.5:
         a += '1'
-    elif mouse_x >=center_x - 45 and mouse_x <= center_x + 45:
+    elif mouse_x >= center_x - 45 and mouse_x <= center_x + 45:
         a += '2'
     elif mouse_x >= center_x + 52.5 and mouse_x <= center_x + 142.5:
         a += '3'
 
     if mouse_y >= center_y - 142.5 and mouse_y <= center_y - 52.5:
         a += '4'
-    elif mouse_y >=center_y - 45 and mouse_y <= center_y + 45:
+    elif mouse_y >= center_y - 45 and mouse_y <= center_y + 45:
         a += '5'
     elif mouse_y >= center_y + 52.5 and mouse_y <= center_y + 142.5:
         a += '6'
-        
+
     if len(a) > 1:
         if a[0] == '1':
             if mouse_x >= center_x - 142.5 and mouse_x <= center_x - 113.5:
@@ -51,8 +53,8 @@ def check_position(mouse_x,mouse_y,chess):
             elif mouse_x >= center_x - 81.5 and mouse_x <= center_x - 52.5:
                 position1 += '3'
                 position2.append(str(center_x - 81))
-            
-        elif a[0] == '2' :
+
+        elif a[0] == '2':
             if mouse_x >= center_x - 45 and mouse_x <= center_x - 16:
                 position1 += '4'
                 position2.append(str(center_x - 44.5))
@@ -62,7 +64,7 @@ def check_position(mouse_x,mouse_y,chess):
             elif mouse_x >= center_x + 16 and mouse_x <= center_x + 45:
                 position1 += '6'
                 position2.append(str(center_x + 16.5))
-                
+
         elif a[0] == '3':
             if mouse_x >= center_x + 52.5 and mouse_x <= center_x + 81.5:
                 position1 += '7'
@@ -73,7 +75,7 @@ def check_position(mouse_x,mouse_y,chess):
             elif mouse_x >= center_x + 113.5 and mouse_x <= center_x + 142.5:
                 position1 += '9'
                 position2.append(str(center_x + 114))
-        
+
         if a[1] == '4':
             if mouse_y >= center_y - 142.5 and mouse_y <= center_y - 113.5:
                 position1 += '1'
@@ -84,8 +86,8 @@ def check_position(mouse_x,mouse_y,chess):
             elif mouse_y >= center_y - 81.5 and mouse_y <= center_y - 52.5:
                 position1 += '3'
                 position2.append(str(center_y - 81))
-            
-        elif a[1] == '5' :
+
+        elif a[1] == '5':
             if mouse_y >= center_y - 45 and mouse_y <= center_y - 16:
                 position1 += '4'
                 position2.append(str(center_y - 44.5))
@@ -95,7 +97,7 @@ def check_position(mouse_x,mouse_y,chess):
             elif mouse_y >= center_y + 16 and mouse_y <= center_y + 45:
                 position1 += '6'
                 position2.append(str(center_y + 16.5))
-                
+
         elif a[1] == '6':
             if mouse_y >= center_y + 52.5 and mouse_y <= center_y + 81.5:
                 position1 += '7'
@@ -107,52 +109,53 @@ def check_position(mouse_x,mouse_y,chess):
                 position1 += '9'
                 position2.append(str(center_y + 114))
 
-            
         if len(position1) > 1 and len(position2) > 1:
-            chess.save(position1,position2)
+            chess.save(position1, position2)
 
-def check_button(screen,retract_button,replay_button,chess,mouse_x,mouse_y):
+
+def check_button(screen, retract_button, replay_button, chess, mouse_x, mouse_y):
     '''检查是否点击按钮'''
-    if retract_button.rect.collidepoint(mouse_x,mouse_y):
+    if retract_button.rect.collidepoint(mouse_x, mouse_y):
         chess.retract()
-    elif replay_button.rect.collidepoint(mouse_x,mouse_y):
+    elif replay_button.rect.collidepoint(mouse_x, mouse_y):
         chess.O1.clear()
         chess.O2.clear()
         chess.X1.clear()
         chess.X2.clear()
 
-def draw(chess,screen):
-	'''绘制图形'''
-	imageX = pygame.image.load('images/X.bmp')
-	imageO = pygame.image.load('images/O.bmp')
-	imageXr = pygame.image.load('images/Xr.bmp')
-	imageOr = pygame.image.load('images/Or.bmp')
-	image_frame = pygame.image.load('images/frame.gif')
-	image_big_frame = pygame.image.load('images/big_frame.gif')
 
-	#绘制普通棋子
-	for position2 in chess.O2:
-		screen.blit(imageO,(float(position2[0]),float(position2[1])))
-	for position2 in chess.X2:
-		screen.blit(imageX,(float(position2[0]),float(position2[1])))
-	
-	#绘制最后一颗棋子
-	if len(chess.X1) > len(chess.O1) :
-		screen.blit(imageXr,(float(chess.X2[-1][0]),float(chess.X2[-1][1])))
-	elif len(chess.X1) == len(chess.O1) and len(chess.X1) > 0:
-		screen.blit(imageOr,(float(chess.O2[-1][0]),float(chess.O2[-1][1])))
-	
-	#绘制下棋区域，注意这里与中心坐标有关，若修改中心坐标，这里同时也要修改
-	last_position = ''  #上一个棋的位置，若无，则空
-	if len(chess.X1) > len(chess.O1):
-		last_position = chess.X1[-1]
-	elif len(chess.X1) == len(chess.O1) and len(chess.X1) > 0:
-		last_position = chess.O1[-1]
-	if last_position:
-		x = (int(last_position[0])-1)%3+1   #x,y为上一个棋对应的宫的相对位置
-		y = (int(last_position[1])-1)%3+1
-		x_position = 100+(x-1)*97.5     #这两个变量为绘制的框的绝对位置
-		y_position = 250+(y-1)*97.5
-		screen.blit(image_frame,(x_position,y_position))
-	else:
-		screen.blit(image_big_frame,(100,250))
+def draw(chess, screen):
+    '''绘制图形'''
+    imageX = pygame.image.load('images/X.bmp')
+    imageO = pygame.image.load('images/O.bmp')
+    imageXr = pygame.image.load('images/Xr.bmp')
+    imageOr = pygame.image.load('images/Or.bmp')
+    image_frame = pygame.image.load('images/frame.gif')
+    image_big_frame = pygame.image.load('images/big_frame.gif')
+
+    # 绘制普通棋子
+    for position2 in chess.O2:
+        screen.blit(imageO, (float(position2[0]), float(position2[1])))
+    for position2 in chess.X2:
+        screen.blit(imageX, (float(position2[0]), float(position2[1])))
+
+    # 绘制最后一颗棋子
+    if len(chess.X1) > len(chess.O1):
+        screen.blit(imageXr, (float(chess.X2[-1][0]), float(chess.X2[-1][1])))
+    elif len(chess.X1) == len(chess.O1) and len(chess.X1) > 0:
+        screen.blit(imageOr, (float(chess.O2[-1][0]), float(chess.O2[-1][1])))
+
+    # 绘制下棋区域，注意这里与中心坐标有关，若修改中心坐标，这里同时也要修改
+    last_position = ''  # 上一个棋的位置，若无，则空
+    if len(chess.X1) > len(chess.O1):
+        last_position = chess.X1[-1]
+    elif len(chess.X1) == len(chess.O1) and len(chess.X1) > 0:
+        last_position = chess.O1[-1]
+    if last_position:
+        x = (int(last_position[0]) - 1) % 3 + 1  # x,y为上一个棋对应的宫的相对位置
+        y = (int(last_position[1]) - 1) % 3 + 1
+        x_position = 100 + (x - 1) * 97.5  # 这两个变量为绘制的框的绝对位置
+        y_position = 250 + (y - 1) * 97.5
+        screen.blit(image_frame, (x_position, y_position))
+    else:
+        screen.blit(image_big_frame, (100, 250))
